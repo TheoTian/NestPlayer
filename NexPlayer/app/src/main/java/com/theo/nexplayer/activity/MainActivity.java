25cc1c26d8e310f10e508d3db766d2c49f817c57
theo <402887075@qq.com>
2018-04-26 10:08:56 +0800

    refactor the code

--- NexPlayer/app/src/main/java/com/theo/nexplayer/activity/MainActivity.java
+++ NexPlayer/app/src/main/java/com/theo/nexplayer/activity/MainActivity.java
@@ -1,17 +1,17 @@
 package com.theo.nexplayer.activity;
 
-import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.theo.nexplayer.NexVideoView;
 import com.theo.nexplayer.jni.NativeMediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String URL = "http://mp4.res.hunantv.com/video/1155/79c71f27a58042b23776691d206d23bf.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         Display display = this.getWindowManager().getDefaultDisplay();
 
         NexVideoView videoView = new NexVideoView(this);
-        ((RelativeLayout)findViewById(R.id.container)).addView(videoView,
+        ((RelativeLayout) findViewById(R.id.container)).addView(videoView,
                 new RelativeLayout.LayoutParams(display.getWidth(), display.getWidth() * 9 / 16));
+        videoView.testPlay(URL);
         Log.d("test", NativeMediaPlayer._test());
 
     }
--- NexPlayer/core/src/main/java/com/theo/nexplayer/NexVideoView.java
+++ NexPlayer/core/src/main/java/com/theo/nexplayer/NexVideoView.java
@@ -1,22 +1,55 @@
 package com.theo.nexplayer;
 
 import android.content.Context;
-import android.os.Environment;
+import android.net.Uri;
 import android.util.AttributeSet;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import com.theo.nexplayer.jni.NativeMediaPlayer;
-
 /**
+ * The simple videoview just for learning
  * Created by txy on 18-4-20.
  */
-
 public class NexVideoView extends SurfaceView {
 
-    private String URL = "http://mp4.res.hunantv.com/video/1155/79c71f27a58042b23776691d206d23bf.mp4";
+    private static final String TAG = "VideoView";
 
-    private NexMediaPlayer mPlayer = new NexMediaPlayer();
+    // all possible internal states
+    private static final int STATE_ERROR = -1;
+    private static final int STATE_IDLE = 0;
+    private static final int STATE_PREPARING = 1;
+    private static final int STATE_PREPARED = 2;
+    private static final int STATE_PLAYING = 3;
+    private static final int STATE_PAUSED = 4;
+    private static final int STATE_PLAYBACK_COMPLETED = 5;
+
+
+    private NexMediaPlayer mMediaPlayer = new NexMediaPlayer();
+
+    // mCurrentState is a VideoView object's current state.
+    // mTargetState is the state that a method caller intends to reach.
+    // For instance, regardless the VideoView object's current state,
+    // calling pause() intends to bring the object to a target state
+    // of STATE_PAUSED.
+    private int mCurrentState = STATE_IDLE;
+    private int mTargetState = STATE_IDLE;
+
+    private int mVideoWidth;
+    private int mVideoHeight;
+
+
+    private int mSurfaceWidth;
+    private int mSurfaceHeight;
+
+    // settable by the client
+    private Uri mUri;
+    private String mUrl;
+
+
+    // All the stuff we need for playing and showing a video
+    private SurfaceHolder mSurfaceHolder = null;
+
 
     public NexVideoView(Context context) {
         super(context);
@@ -34,24 +67,45 @@ public class NexVideoView extends SurfaceView {
     }
 
     private void initView(Context context) {
-        URL = context.getExternalCacheDir().getAbsolutePath() + "/test.mp4";
+        mVideoWidth = 0;
+        mVideoHeight = 0;
+
+        mSurfaceWidth = 0;
+        mSurfaceHeight = 0;
+
         getHolder().addCallback(mSHCallback);
 
+        mCurrentState = STATE_IDLE;
+        mTargetState = STATE_IDLE;
+
     }
+
 
     SurfaceHolder.Callback mSHCallback = new SurfaceHolder.Callback() {
         public void surfaceChanged(SurfaceHolder holder, int format,
                                    int w, int h) {
-
         }
 
         public void surfaceCreated(final SurfaceHolder holder) {
-            mPlayer.testPlay(URL, holder.getSurface());
+            mSurfaceHolder = holder;
+            openVideo();
         }
 
         public void surfaceDestroyed(SurfaceHolder holder) {
         }
     };
 
+    public void testPlay(String url) {
+        mUri = Uri.parse(url);
+        mUrl = url;
+    }
 
+    private void openVideo() {
+        if (mUri == null || mSurfaceHolder == null) {
+            // not ready for playback just yet, will try again later
+            return;
+        }
+
+        mMediaPlayer.testPlay(mUrl, mSurfaceHolder.getSurface());
+    }
 }
